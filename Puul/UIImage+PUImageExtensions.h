f64f15e632ac47fe73bdfae42e3c445fe12b43bb
SNIP3ROV3RLORD420 <dylanhumphrey@Dylans-MacBook.local>
2015-03-25 14:57:06 -0700

    Made project work/Added image extension

---
+++ Puul/UIImage+PUImageExtensions.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+PUImageExtensions.h
+//  Puul
+//
+//  Created by Dylan Humphrey on 3/25/15.
+//  Copyright (c) 2015 Dylan Humphrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage_PUImageExtensions : UIImage
+
+@end
---
+++ Puul/UIImage+PUImageExtensions.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+PUImageExtensions.m
+//  Puul
+//
+//  Created by Dylan Humphrey on 3/25/15.
+//  Copyright (c) 2015 Dylan Humphrey. All rights reserved.
+//
+
+#import "UIImage+PUImageExtensions.h"
+
+@implementation UIImage_PUImageExtensions
+
+@end
