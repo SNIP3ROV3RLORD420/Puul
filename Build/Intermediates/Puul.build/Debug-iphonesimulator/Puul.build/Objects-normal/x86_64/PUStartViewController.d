e0512e7b2b9c4267f4ee3af500cd6ebb909d057f
SNIP3ROV3RLORD420 <dylanhumphrey@dylans-macbook.internal.hws.org>
2015-03-26 09:11:28 -0700

    deleted crap that should not be in the repo

--- Build/Intermediates/Puul.build/Debug-iphonesimulator/Puul.build/Objects-normal/x86_64/PUStartViewController.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/dylanhumphrey/Desktop/Drive/XCode/Puul/Puul/PUStartViewController.m \
-  /Users/dylanhumphrey/Desktop/Drive/XCode/Puul/Puul/PUStartViewController.h \
-  /Users/dylanhumphrey/Desktop/Drive/XCode/Puul/Puul/PULoginViewController.h \
-  /Users/dylanhumphrey/Desktop/Drive/XCode/Puul/Puul/PUSignUpViewController.h
